136cf5e8e906ba88b85032e9849c2b6f1ed3b7ff
ROHIT CHAUHAN <rc672800@gmail.com>
2022-06-17 16:23:09 +0530

    sidebar work in progress

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,9 +3,29 @@ import "./sidebar.scss";
 
 const Sidebar = () => {
   return (
-    <div className="Sidebar">
-      <div className="top">logo</div>
-      <div className="center">list</div>
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">lamaadmin</span>
+      </div>
+      <div className="center">
+        <ul>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+        </ul>
+      </div>
       <div className="bottom">color options</div>
     </div>
   );
